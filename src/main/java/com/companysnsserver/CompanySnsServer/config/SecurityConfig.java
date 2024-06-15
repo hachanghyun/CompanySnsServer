646537b76306749398e566048f41d5ba94de2d6c
hachanghyun <haccwx@naver.com>
2024-06-15 22:35:36 +0900

    commit

--- src/main/java/com/companysnsserver/CompanySnsServer/config/SecurityConfig.java
+++ src/main/java/com/companysnsserver/CompanySnsServer/config/SecurityConfig.java
@@ -17,7 +17,8 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
-                                .requestMatchers("/api/register", "/api/login", "/file/upload", "/api/profile/**", "/image/upload","/api/images/**" ,"/api/followingPosts/", "/api/search", "/api/follow" , "/api/followers" , "/api/unfollow" , "/api/isFollowing" , "/api/connections" , "/api/allPosts" ).permitAll() // 인증 없이 접근 가능
+                                .requestMatchers("/api/**", "/file/upload", "/image/upload" ).permitAll() // 인증 없이 접근 가능
+                                //.requestMatchers("/api/register", "/api/login", "/file/upload", "/api/profile/**", "/image/upload","/api/images/**" ,"/api/followingPosts/", "/api/search", "/api/follow" , "/api/followers" , "/api/unfollow" , "/api/isFollowing" , "/api/connections" , "/api/allPosts" ).permitAll() // 인증 없이 접근 가능
                                 .anyRequest().authenticated() // 다른 모든 요청은 인증 필요
                 );
         return http.build();
